81db804616720a4538df04e7e8b3595f56003e39
Ben Mendis <dragonwisard@gmail.com>
2012-02-29 16:51:28 -0500

    Basic framework for distributing our comparison jobs

--- src/main/scala/com/antennahouse/us/taskmaster/TaskmasterClient.scala
+++ src/main/scala/com/antennahouse/us/taskmaster/TaskmasterClient.scala
@@ -1,14 +1,40 @@
 package com.antennahouse.us.taskmaster
 
+import akka.actor.Actor
 import akka.actor.Actor._
+import akka.actor.ActorRef
 
 object TaskmasterClient {
+  var taskmasterServiceActor: ActorRef = null
+
   def main(args: Array[String]) {
-    val actor = remote.actorFor("taskmaster-service", "localhost", 2552)
-    println("Sending 'Hello'.")
-    val result = (actor ? "Hello").as[String]
-    println("Received: " + result)
-    System exit 0
+    remote.start("localhost", 2553)
+    taskmasterServiceActor = remote.actorFor("taskmaster-service", "localhost", 2552)
+    val localActor = actorOf[TaskmasterClientActor]
+    localActor.start()
+    localActor ! JobRequest
+  }
+
+  class TaskmasterClientActor extends Actor {
+    def receive = {
+      case JobsFinished =>
+        println("No more jobs to do.")
+        Runtime.getRuntime().halt(0)
+      case Job(data) =>
+        // Process data
+        println("Processing " + data._1 + " and " + data._2)
+        Thread sleep 1000
+        self reply JobResult(data)
+        requestAJob()
+      case JobRequest =>
+        println("received JobRequest.")
+        requestAJob()
+    }
+
+    def requestAJob() {
+      println("Requesting a Job.")
+      taskmasterServiceActor ! JobRequest
+    }
   }
 }
 
--- src/main/scala/com/antennahouse/us/taskmaster/TaskmasterService.scala
+++ src/main/scala/com/antennahouse/us/taskmaster/TaskmasterService.scala
@@ -2,19 +2,75 @@ package com.antennahouse.us.taskmaster
 
 import akka.actor.Actor
 import akka.actor.Actor._
+import java.io.{File, FilenameFilter}
 
-class TaskmasterServiceActor extends Actor {
-  def receive = {
-    case "Hello" =>
-        println("Received 'Hello'.")
-        self.reply("World")
-  }
-}
+sealed trait TaskMessage
+case object JobRequest extends TaskMessage
+case object JobsFinished extends TaskMessage
+case class Job(data: (File,File)) extends TaskMessage
+case class JobResult(data: (File,File)) extends TaskMessage
 
 object TaskmasterService {
+  var compare_list: List[(File, File)] = Nil
+
   def main(args: Array[String]) {
+    if (args.length < 1) {
+      printUsage()
+      System exit 1
+    }
+    val input_dir = new File(args(0))
+    if (!input_dir.isDirectory) {
+      printUsage()
+      System exit 2
+    }
+    val test_docs = input_dir.listFiles(new FilenameFilter() {
+        def accept(dir: File, name: String): Boolean = {
+          return name.toLowerCase().endsWith(".xml")
+        }
+      })
+    var test_docs_list = test_docs.toList
+    test_docs_list = test_docs_list sortWith {_.toString() < _.toString()}
+    var even = false
+    var odd: File = null
+    for ( file <- test_docs_list ) {
+      if (!even) {
+        odd = file
+        even = true
+      } else {
+        println(odd.toString() + " " + file.toString())
+        compare_list = (odd, file) :: compare_list
+        even = false
+      }
+    }
+    compare_list = compare_list.reverse
     remote.start("localhost", 2552)
     remote.registerPerSession("taskmaster-service", actorOf[TaskmasterServiceActor])
+  }
+
+  def printUsage() {
+    println("You're doing it wrong.")
+  }
+
+  class TaskmasterServiceActor extends Actor {
+    var sent = 0
+    var received = 0
+
+    def receive = {
+      case JobRequest =>
+        println("Got a JobRequest.")
+        if (compare_list == Nil) {
+          println("No more jobs.")
+          self reply JobsFinished
+        } else {
+          println("Sending a job.")
+          self reply Job(compare_list.head)
+          compare_list = compare_list.tail
+          sent += 1
+        }
+      case JobResult(data) =>
+        println("Received a JobResult.")
+        received += 1
+    }
   }
 }
 
