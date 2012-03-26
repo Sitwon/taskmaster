d98392c5988e545c4c20a7bc49fbc8b9b058e3eb
Ben Mendis <dragonwisard@gmail.com>
2012-03-26 13:44:25 -0400

    Creating more generic messages

--- src/com/antennahouse/us/taskmaster/Messages.scala
+++ src/com/antennahouse/us/taskmaster/Messages.scala
@@ -6,8 +6,13 @@ object Messages {
   sealed trait TaskMessage
   case object JobRequest extends TaskMessage
   case object JobsFinished extends TaskMessage
+
   case class Job(data: (File,File)) extends TaskMessage
   case class JobResult(data: (File,File)) extends TaskMessage
   case class AddJob(data: (File,File)) extends TaskMessage
+
+  case class GenJob[D,R](data: D, task: D => R) extends TaskMessage
+  case class GenJobResult[D,R](data: D, result: R) extends TaskMessage
+  case class GenAddJob(job: GenJob[_,_]) extends TaskMessage
 }
 
